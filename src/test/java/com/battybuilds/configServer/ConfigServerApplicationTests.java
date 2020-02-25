b903c72623e47610e3c878ba7196e7a54b11118c
Tim B <tbatty7@gmail.com>
2020-02-24 20:40:28 -0500

    initial commit setting up config server

---
+++ src/test/java/com/battybuilds/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battybuilds.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
